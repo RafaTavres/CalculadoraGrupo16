787da815e0e815cd0e6aa2b87b8b752c444b961d
Rafael S Tavares <ragbriros@gmail.com>
2023-06-07 14:17:22 -0300

    Add project files.

---
+++ CalculadoraGrupo16.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraGrupo16.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
